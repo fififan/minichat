6d0db2bc8580dc191cd14a049d16f52e9ad76c54
fififan <nigues.f@hotmail.com>
2018-11-08 13:59:00 +0100

    index.php

--- tchat/index.php
+++ tchat/index.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="utf-8" />
         <title>Mini-chat</title>
+        <script type="text/javascript" src="lib/js/jquery.js"></script>
+        <script type="text/javascript" src="lib/js/bootstrap.js"></script>
+        <link rel="stylesheet" type="text/css" href="lib/css/bootstrap.css">
     </head>
     <style>
     form
@@ -11,42 +14,69 @@
     }
     </style>
     <body>
-    
-    <form action="index.php" method="post">
-        <p>
-        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
-        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
 
-        <input type="submit" value="Envoyer" />
-	</p>
-    </form>
+        <form action="index.php" method="post">
+            <p>
+                <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
+                <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
+
+                <input type="submit" value="Envoyer" />
+	       </p>
+        </form>
 
 <?php
 // Connexion à la base de données
 
-try
-{
+
+try{
 	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
 }
-catch(Exception $e)
+    catch(Exception $e)
 {
         die('Erreur : '.$e->getMessage());
 }
 
-// Récupération des 10 derniers messages
-$reponse = $bdd->prepare('SELECT * FROM minitchat left join(utilisateur) on utilisateur.id = minitchat.utilisateurId);
-$reponse->execute();
 
-// Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
 
-while ($donnees = $reponse->fetch())
-{
-    echo $donnees['nom'] . " a dit : " . $donnees['message'] . "<br />";
-	
+
+    $reponse = $bdd->prepare('INSERT INTO utilisateur (nom, prenom, email, motdepasse) vALueS (:nom, :prenom, :email, :motdepasse)');
+    $reponse->bindParam(':nom', $_POST["nom"]);
+    $reponse->bindParam(':prenom', $_POST["prenom"]);
+    $reponse->bindParam(':email', $_POST["email"]);
+    $reponse->bindParam(':motdepasse', $_POST["motdepasse"]);
+    $reponse->execute();
+
+    if (isset($_POST["message"])) {
+   $req = $bdd->prepare('INSERT INTO minichat (message, utilisateurId) VALUES (:message, :userId)');
+   $req->bindParam(':message', $_POST["message"]);
+   $req->bindParam(':userId', $_SESSION['userid']);
+   $req->execute();
 }
 
-$reponse->closeCursor();
+    // Récupération des 10 derniers messages
+    $reponse = $bdd->prepare('SELECT * 
+                              FROM minitchat 
+                              LEFT JOIN utilisateur 
+                                ON minitchat.utilisateurId = utilisateur.id;
+    ');
+    $reponse->execute();
 
+    // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
+
+    while ($donnees = $reponse->fetch()){
+        echo $donnees['nom'] . " a dit : " . $donnees['message'] . "<br />";
+	
+    }
+
+    $reponse->closeCursor();
+
+    if(isset($_SESSION['userid'])) {
+
+}
+
+if(isset($_POST['logout'])) {
+
+}
 ?>
     </body>
 </html>
